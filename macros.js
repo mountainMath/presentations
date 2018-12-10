21931a35079203268999ca219ecf95d657f06e07
Jens von Bergmann <jens@mountainmath.ca>
2018-12-10 10:38:34 -0800

    reproducible housing data talk

--- macros.js
+++ macros.js
@@ -1,4 +1,4 @@
-remark.macros.scale = function (percentage) {
+remark.macros.scale_image = function (percentage) {
   var url = this;
   return '<img src="' + url + '" style="width: ' + percentage + '" />';
 };
